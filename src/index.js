d3a79f201876a876ea9a1392e8ca6c1a33b46c82
ISIOMA123123 <ubanisuccess26@gmail.com>
2025-01-19 20:39:43 +0100

    improved the user experience

--- src/index.js
+++ src/index.js
@@ -14,9 +14,13 @@ function generatePoem(event) {
 let userPoemTopicElement = document.querySelector("#user-poem-topic");
 
    let apiKey = "7d3fo7t42ac4a88b5f1b6383fbbe2b0b";
-   let context = "you are a romantic poem expert and love to write short poem. your mission is to generate a four lines poem in basic html, and seperate each line with a <br />, make sure to follow the user instructions. Sign the poem with 'Success' in bold inside a strong element at the end of the poem";
+   let context = "you are a romantic poem expert and love to write short poem. your mission is to generate a four lines poem in basic '<html><html/>', and seperate each line with a <br />, make sure to follow the user instructions. Sign the poem with 'Success' in bold inside a strong element at the end of the poem";
    let prompt = `user instructions: generate a poem about ${userPoemTopicElement.value}`;
    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+   let poemElement = document.querySelector("#poem");
+poemElement.classList.remove("hidden");
+poemElement.innerHTML = `<div class="blink"> generating poem about ${userPoemTopicElement.value}......</div>`;
 
 console.log("generating prompt");
    console.log(`prompt: ${prompt}`);
