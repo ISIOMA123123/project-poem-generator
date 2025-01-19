2ae002fc2a1b512f79a8a90084f58fdc72df69ab
ISIOMA123123 <ubanisuccess26@gmail.com>
2025-01-19 20:51:18 +0100

    i removed some words from the context

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function generatePoem(event) {
 let userPoemTopicElement = document.querySelector("#user-poem-topic");
 
    let apiKey = "7d3fo7t42ac4a88b5f1b6383fbbe2b0b";
-   let context = "you are a romantic poem expert and love to write short poem. your mission is to generate a four lines poem in basic '<html><html/>', and seperate each line with a <br />, make sure to follow the user instructions. Sign the poem with 'Success' in bold inside a strong element at the end of the poem";
+   let context = "you are a romantic poem expert and love to write short poem. your mission is to generate a six lines poem, and seperate each line with a <br />, make sure to follow the user instructions. Sign the poem with 'Success' in bold inside a strong element at the end of the poem";
    let prompt = `user instructions: generate a poem about ${userPoemTopicElement.value}`;
    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
