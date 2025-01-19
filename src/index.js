47ff01c369c4ef878b95aee3dc7ae358f08277db
ISIOMA123123 <ubanisuccess26@gmail.com>
2025-01-19 16:40:58 +0100

    added the basic form handling and typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-alert("hello world"); 
+function generatePoem(event) {
+    event.preventDefault();
+    
+
+     new Typewriter("#poem", {
+        strings: "my name is success ubani chinedu",
+        autoStart: true,
+        cursor: "",
+        delay: 1,
+    });
+
+
+}
+
+let poemFormElement = document.querySelector("#poemGeneratorForm");
+console.log(poemFormElement);
+poemFormElement.addEventListener("submit", generatePoem);
+
